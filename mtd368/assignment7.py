4700c2a932cd0616b71da0a79e8c827b67985078
Matthew Dunn <mattyd2@gmail.com>
2015-11-01 18:17:25 -0500

    up to question 4

--- mtd368/assignment7.py
+++ mtd368/assignment7.py
@@ -1,34 +1,15 @@
 import sys
 import array as ar
-from array import twoDArray
-
-# This try/except design I discussed with Sida because I was struggling with how to start the program. I tride mimicking what was in Adventure and failed.
+from array import arraybuilder, twoDArray, thedivider, arraymath
 
 try:
-    array = ar.twoDArray
+	question1 = twoDArray()
+	question2 = thedivider()
+	question3 = arraymath()
 except Exception as e: raise
-        # if userWantsToQuit != None:
-        #     sys.exit()
-	
-        #     splitinput = userinput.split(', ')
-        #     acceptableInterval = [Interval(i) for i in splitinput]
-        #     break
 
-
-#     while True:
-#         userinput = raw_input('Intervals? ')
-#         userWantsToQuit = re.match("quit", userinput)
-#         if userWantsToQuit != None:
-#             sys.exit()
-#         try:
-#             newInterval = Interval(userinput)
-#     except Exception as e: raise
-#         else:
-#             acceptableInterval = insert(acceptableInterval, newInterval)
-#             printableListOfIntervalsAsStrings = [str(i) for i in acceptableInterval]
-#             print ', '.join(printableListOfIntervalsAsStrings)
 
 except KeyboardInterrupt, ValueError:
-    print "\n Interrupted!"
+     print "\n Interrupted!"
 except EOFError:
-    print "\n Interrupted!"
+     print "\n Interrupted!"
